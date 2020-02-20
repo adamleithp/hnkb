3b5d1ea9adf3a963782d5594b6a1e96c666641aa
adamleithp <adamleithp@gmail.com>
2020-02-19 23:14:40 -0400

    Comment loading! Navigation after initial down press are blocked

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loadingElement = document.getElementById('loading')
 const storyTitleElement = document.getElementById('story-title')
 const URL = document.getElementById('story-url')
 const TEXT = document.getElementById('story-text')
-
+// const COMMENTS = document.getElementById('story-comments')
 
 const getStory = async (storyID) => {  
   const story = await fetch(`https://hacker-news.firebaseio.com/v0/item/${storyID}.json?print=pretty`)
@@ -16,6 +16,92 @@ const getStory = async (storyID) => {
   return story;
 }
 
+const buildCommentHTML = (commentObject) => {
+  let author = commentObject.by;
+  let text = commentObject.text;
+
+  // Validation
+  if (!commentObject) return false;
+  if (commentObject.deleted === true) return false;
+
+  // create div
+  const parent = document.createElement('div');
+  // div.classList.add('story-comment');
+  
+  // create author p
+  const child1 = document.createElement('p');
+  child1.innerText = author;
+  
+  // create author text
+  const child2 = document.createElement('div');
+  child2.innerHTML = text;
+  
+  parent.appendChild(child1)
+  parent.appendChild(child2)
+    
+  return parent;
+}
+
+const renderComments = async (commentsArray, elementId = null) => {
+
+  console.log('commentsArray======', typeof commentsArray);
+  
+  const COMMENT_PARENT = document.getElementById('story-comments');
+
+    
+  // Remove comments if incoming array is empty.
+  if (!commentsArray && commentsArray.length === 0) {
+    COMMENT_PARENT.innerHTML = '';
+    return;
+  }
+
+  const isNested = elementId ? true : false;
+  const PARENT = elementId ? document.getElementById(elementId) : COMMENT_PARENT;  
+
+  let comments = await Promise.all(
+    commentsArray.map(async commentID => {
+      let commentsResponse = await getStory(commentID)
+      return commentsResponse;
+    })
+  )
+  
+  // Handle HTML 
+  comments.forEach((comment) => {
+    // Validation
+    if (!comment) return;
+    if (comment.deleted === true) return;
+    
+    // Create the list item
+    const LIST_ITEM = document.createElement('li');
+    
+    // Styling
+    LIST_ITEM.classList.add('story-comment');
+    // Set list item content:
+    LIST_ITEM.id = comment.id; 
+    
+    // Build List Item content
+    const COMMENT_CONTENT = buildCommentHTML(comment)
+    LIST_ITEM.appendChild(COMMENT_CONTENT);
+
+    // If nested, create another <ul> and put the comment in there.
+    if (isNested) {
+      const LIST = document.createElement('ul');
+      LIST.classList.add('list-comment');
+
+      PARENT.appendChild(LIST);
+      LIST.appendChild(LIST_ITEM)
+    }  else {
+      // Append list items to ul
+      PARENT.appendChild(LIST_ITEM);
+    }
+
+    if (comment.kids) {
+      // Recursion
+      renderComments(comment.kids, comment.id)
+    }
+  })
+}
+
 
 const renderCurrentStory = async (storyArray) => {
   const currentStoryIndex = localStorage.getItem('currentStoryIndex');
@@ -23,8 +109,10 @@ const renderCurrentStory = async (storyArray) => {
   const story = await getStory(currentStoryID)  
 
   let hasText = story.text === undefined ? false : true;
+  let hasComments = story.kids && story.kids.length >= 1 ? true : false;
   let content = hasText ? story.text : story.url
   
+  // Set title html
   storyTitleElement.innerHTML = story.title;
 
   if (hasText) {
@@ -43,6 +131,15 @@ const renderCurrentStory = async (storyArray) => {
     URL.classList.remove('hide')
     URL.focus();
   }
+
+  if (hasComments) {  
+    localStorage.setItem('currentStoryComments', story.kids)
+  }
+
+  // if (currentStoryComments) {
+  //   let array = currentStoryComments.split(",")
+  //   renderComments(array)
+  // }
 }
 
 
@@ -52,17 +149,22 @@ const attachEventListeners = (storiesArray) => {
   document.onkeydown = (e) => {
     let totalStoryIndex = localStorage.getItem('topStoriesLength');
     let currentStoryIndex = localStorage.getItem('currentStoryIndex');
+    let currentStoryComments = localStorage.getItem('currentStoryComments');
+    let commentsAreRenderedFlag = localStorage.getItem('commentsAreRenderedFlag');
     
     // LEFT
     if (e.keyCode == '37') {
       if (currentStoryIndex < 1) {        
         return;
       }
-
+      
       let newIndex = Number(currentStoryIndex) - 1
-      const newStoryIndex = localStorage.setItem('currentStoryIndex', newIndex);
+      localStorage.setItem('currentStoryIndex', newIndex);
+      localStorage.removeItem('currentStoryComments');
+      localStorage.removeItem('commentsAreRenderedFlag');
       
       renderCurrentStory(stories);
+      renderComments([]);
     }
     
     
@@ -73,10 +175,22 @@ const attachEventListeners = (storiesArray) => {
       }
 
       let newIndex = Number(currentStoryIndex) + 1
-      const newStoryIndex = localStorage.setItem('currentStoryIndex', newIndex);
-      
-
+      localStorage.removeItem('currentStoryComments');
+      localStorage.removeItem('commentsAreRenderedFlag');
+      localStorage.setItem('currentStoryIndex', newIndex);
+            
       renderCurrentStory(stories);
+      renderComments([]);
+    }
+
+
+    // DOWN (render comments)
+    else if (e.keyCode == '40' && !commentsAreRenderedFlag) {
+      const commentsArray = currentStoryComments.split(',')
+      
+      renderComments(commentsArray);
+
+      localStorage.setItem('commentsAreRenderedFlag', true);
     }
   };
 }
@@ -111,6 +225,5 @@ const getTopStories = async (storyID) => {
 (async function() { 
   const storiesArray = await getTopStories()
   await renderCurrentStory(storiesArray)
-
   const action = attachEventListeners(storiesArray)  
 })();
